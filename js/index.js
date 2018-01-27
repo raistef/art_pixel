d60a5679b526670aa7461d21d6952f831f6aad8b
stefanyrl <raileanu_stefany@yahoo.com>
2018-01-27 21:21:21 +0200

    add drag funct

--- js/index.js
+++ js/index.js
@@ -31,7 +31,43 @@ function makeGrid() {
   $(this).css("background-color", color);
  
 });
+
+///// https://github.com/Syknapse/Pixel-art-maker/blob/master/designs.js - source for the following block of code ///
+
+	var color=document.getElementById("colorPicker").value;
+    let mouseIsDown = true;
+    let clicks = $(this).data('clicks');
+    $('table').on('mouseleave', 'td', function(){
+		color=document.getElementById("colorPicker").value;
+        if (mouseIsDown){
+            if (!clicks){
+                // Change background color of cell
+                $(this).css('background-color', color);
+            } else {
+                // On second click return color to default (erase)
+                $(this).css('background-color', '');
+            }
+            // Fire `if` event on odd clicks
+            $(this).data('clicks', !clicks);
+        }
+    })
+    $('table').on('mousedown', 'td', function(){
+        event.preventDefault();
+        mouseIsDown = true;
+    })
+    $('table').on('mouseup', 'td', function(){
+        mouseIsDown = false;
+    });
+    $('table').on('mouseleave', function(){
+        mouseIsDown = false;
+    });
+
+
+/////////////////////////////////////////////////////
+
 }
+
+
 
 
 
