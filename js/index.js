1e28abb3c59b3120c3abcdbbab39ffa77601e139
stefanyrl <raileanu_stefany@yahoo.com>
2018-04-30 00:40:03 +0300

    change

--- js/index.js
+++ js/index.js
@@ -1,7 +1,65 @@
 
+	var h="";
+	  for (var i=0; i<100; i++) {
+	 h+="<tr>";
+	  for (var j=0; j<200; j++) {
+		h+="<td></td>";
+		
+	  }
+	  h+="</tr>";
+	}
+
+
+  
+	$("table").html(h);
+	$("table").css("background-color", "white");
+
+	  $("#pixel_canvas tr td").click(function(){    
+  var color=document.getElementById("colorPicker").value;
+  $(this).css("background-color", color);
+ 
+});
+
+
+///// https://github.com/Syknapse/Pixel-art-maker/blob/master/designs.js - source for the following block of code ///
+
+	var color=document.getElementById("colorPicker").value;
+    let mouseIsDown = false;
+    let clicks = $(this).data('clicks');
+    $('table').on('mouseleave', 'td', function(){
+		color=document.getElementById("colorPicker").value;
+        if (mouseIsDown){
+            if (!clicks){
+                // Change background color of cell
+                $(this).css('background-color', color);
+            } else {
+                // On second click return color to default (erase)
+                $(this).css('background-color', '');
+            }
+            // Fire `if` event on odd clicks
+            $(this).data('clicks', !clicks);
+        }
+    })
+    $('table').on('mousedown', 'td', function(){
+        event.preventDefault();
+        mouseIsDown = true;
+    })
+    $('table').on('mouseup', 'td', function(){
+        mouseIsDown = false;
+    });
+    $('table').on('mouseleave', function(){
+        mouseIsDown = false;
+    });
+
+
+/////////////////////////////////////////////////////
+
+
+////////////
+
+
+
 $("#sizePicker").submit(function(event) {
-  //var height = $("#input_height");
-  //console.log(height);
   makeGrid();
   event.preventDefault();
 });
@@ -21,6 +79,8 @@ function makeGrid() {
   }
   h+="</tr>";
 }
+
+
   
 	$("table").html(h);
 	$("table").css("background-color", "white");
